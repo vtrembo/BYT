784e004ce09c4ef016d26c608fc541780eb96670
Valerii <s20970@pjwstk.edu.pl>
2022-09-22 22:54:17 +0200

    Summarized

---
+++ BYT910/Models/Player.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BYT910.Models
+{
+    public class Player
+    {
+        public int Name { get; set; }
+        public int MaxHealthPoints { get; set; }
+        public int CurrentHealthPoints { get; set; }
+        public int Level { get; set; }  
+        public string Class { get; set; }
+        public int StoryCompleted { get; set; }
+
+    }
+}
